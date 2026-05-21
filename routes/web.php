f54fae91331e760a7033813eccd250b550555a2a
Gustavo Araujo (SEDESE) <x22054056@ca.mg.gov.br>
2026-05-21 13:34:23 -0300

    config ldap

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ require __DIR__.'/auth.php';
 
 //testes
 Route::get('/ldap-test', function () {
-    $user = \App\Ldap\User::where('mail', 'testedasilva@gmail.com')->first();
+    $user = \App\Ldap\User::where('email', 'testedasilva@gmail.com')->first();
     dd($user);
 });
