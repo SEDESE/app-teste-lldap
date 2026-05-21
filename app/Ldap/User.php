8129e5c39c79af23bd5ef8785c51df9c5d36b418
Gustavo Araujo (SEDESE) <x22054056@ca.mg.gov.br>
2026-05-21 11:23:44 -0300

    config ldap

--- app/Ldap/User.php
+++ app/Ldap/User.php
@@ -3,9 +3,13 @@
 namespace App\Ldap;
 
 use LdapRecord\Models\Model;
+use LdapRecord\Laravel\Auth\AuthenticatesWithLdap;
+use LdapRecord\Laravel\Auth\LdapAuthenticatable;
 
-class User extends Model
+class User extends Model implements LdapAuthenticatable
 {
+    use AuthenticatesWithLdap;
+
     public static array $objectClasses = [
         'inetOrgPerson',
     ];
